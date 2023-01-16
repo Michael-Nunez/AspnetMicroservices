f4c59ead09c82ed202f2095dcf14aacc51cea985
Michael Nunez <michael.nunez@uepa.com>
2023-01-16 15:01:45 -0400

    Refactoring Models Folder  And Creating Services Folder and the Interfaces and classes into it.

---
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,48 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel basketModel)
+        {
+            var response = await _httpClient.PostAsJson("/Basket/Checkout", basketModel);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling the API.");
+            }
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _httpClient.GetAsync($"/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel basketModel)
+        {
+            var response = await _httpClient.PostAsJson("/Basket", basketModel);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<BasketModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling the API.");
+            }
+        }
+    }
+}
---
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -0,0 +1,51 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CatalogModel> CreateCatalog(CatalogModel catalogModel)
+        {
+            var response = await _httpClient.PostAsJson($"/Catalog", catalogModel);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<CatalogModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong calling API");
+            }
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var response = await _httpClient.GetAsync("/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var response = await _httpClient.GetAsync($"/Catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await _httpClient.GetAsync($"/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+    }
+}
